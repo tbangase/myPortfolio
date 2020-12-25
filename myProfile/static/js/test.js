b1aa13a81c2cd13a5d5b02d0568ff1efe6b5f117
tbangase <ichi.toshi217@gmail.com>
2020-12-25 18:00:15 +0900

    modify lateral scroll cards and add menu bar

--- myProfile/static/js/test.js
+++ myProfile/static/js/test.js
@@ -1,6 +1,32 @@
 $(function() {
+    function scrollToAnchor(id) {
+        debugger;
+        var aTag = $(id);
+        $('html,body').animate({scrollTop: aTag.offset().top}, 'slow');
+    }
+
     $('h1').on('click', function () {
         alert('Clickされました');
+    });
+
+    $("#contactLink").on('click', function(){
+        scrollToAnchor('#contact');
+    });
+
+    $("#portfolioLink").on('click', function(){
+        scrollToAnchor('#portfolio');
+    });
+
+    $("#skillLink").on('click', function(){
+        scrollToAnchor('#skill');
+    });
+
+    $("#profileLink").on('click', function(){
+        scrollToAnchor('#profile');
+    });
+
+    $("#topLink").on('click', function(){
+        scrollToAnchor('#top');
     });
 });
 
@@ -9,9 +35,13 @@ $(window).on('scroll', function(){
     var scrollTop = $(window).scrollTop();
     var bgPosition = scrollTop / 1.5;
 
-    debugger;
   
     if(bgPosition){
       $('#top').css('background-position', 'center bottom -'+ bgPosition + 'px');
     }
 });
+
+
+
+
+
