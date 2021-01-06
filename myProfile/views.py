87b5cd57f877d843b0c3ca0126f241025cbfc43e
tbangase <ichi.toshi217@gmail.com>
2021-01-06 18:06:26 +0900

    add message after inquiry send. and modifying email sending.

--- myProfile/views.py
+++ myProfile/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def top(request):
     return render(request, 'top.html')
+
+def inquiry(request):
+    send_mail(
+        'お問い合わせ', 
+        'テスト',
+        'example@mail.com',
+        ['toshiki.ichibangase.7853@gmail.com']
+    )
+    messages.info(request, 'お問い合わせを送信しました')
+    return redirect('/')
